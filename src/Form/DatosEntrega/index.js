78d8c6389fe10e310ffcae58061d632971b08447
Axel <axel.man94gmail.com>
2023-05-25 16:31:14 -0600

    Terminando las validaciones en los demas steps

--- src/Form/DatosEntrega/index.js
+++ src/Form/DatosEntrega/index.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarInput } from "./validaciones";
 
-const DatosEntrega = () => {
+const DatosEntrega = ( {updateStep} ) => {
+  const [address, setAddress] = useState({
+    value: '',
+    valid: null,
+  })
+
+  const [city, setCity] = useState({
+    value: '',
+    valid: null,
+  })
+
+  const [province, setProvince] = useState({
+    value: '',
+    valid: null,
+  })
+
   return (
     <Box
       component="form"
@@ -12,6 +28,11 @@ const DatosEntrega = () => {
         justifyContent: "center",
         flexDirection: "column",
       }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        updateStep(3);
+        console.log(city, address, province)
+      }}
     >
       <TextField
         label="Dirección"
@@ -19,6 +40,14 @@ const DatosEntrega = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={address.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setAddress({ value, valid })
+        }}
+        error={address.valid === false}
+        helperText={address.valid === false && "Ingresa al menos 4 caracteres"}
       />
       <TextField
         label="Ciudad"
@@ -26,6 +55,14 @@ const DatosEntrega = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={city.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setCity({ value, valid })
+        }}
+        error={city.valid === false}
+        helperText={city.valid === false && "Ingresa al menos 4 caracteres"}
       />
       <TextField
         label="Estado/Provincia"
@@ -33,6 +70,14 @@ const DatosEntrega = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={province.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setProvince({ value, valid })
+        }}
+        error={province.valid === false}
+        helperText={province.valid === false && "Ingresa al menos 4 caracteres"}
       />
       <Button variant="contained" type="submit">
         Crear cuenta
